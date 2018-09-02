d4fd161ac285c70e3e75d355242bec24fb580406
guxiangyuan11 <xue673867504@163.com>
2018-09-02 22:54:47 +0800

    上传了图片等资源，以及写了部分主页面静态页面，添加sass支持，swiper组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
+// 路由器对象
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import VueRouter from 'vue-router'
+import Msite from '../pages/Msite/Msite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'// 订单组件页面组件
+import Profile from '../pages/Profile/Profile.vue'// 个人页面
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
+export default new VueRouter({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/msite',
+      component: Msite
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/profile',
+      component: Profile
     }
   ]
 })
